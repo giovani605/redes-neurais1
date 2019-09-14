4ee6ec454fb8e99fc833a9fc4e815a75ebc060a5
Giovani Bossoni <giovanibossoni@hotmail.com>
2019-09-14 17:28:55 -0300

    Tentando implementar adaline

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# In[]
 import numpy as np #Biblioteca de manipulacao de arrays Numpy
 
 ###Construindo Adaline -- minha base
+
+
 class Adaline(object):
     def __init__(self, eta = 0.001, epoch = 100):
         self.eta = eta
@@ -103,18 +106,114 @@ class MyPercetron(object):
     def printVetorPesos(self):
         for v in self.vetorPesos:
             print(v)
-    
 
-redeAdaline =  Adaline(eta = 0.01)
-redePercetron = Perceptron(1)
 
-myPercepetron = MyPercetron(2,2)
-myPercepetron.printVetorPesos()
-vetorEntrada = [1,1]
-resultado = myPercepetron.predict(vetorEntrada)
-print(resultado)
+
+
+
+# In[]
+
+# Vetor entrada
+#vetor resposta
+perceptronOr = Perceptron(2)
+percetronAnd = Perceptron(2)
 
 matrizX = np.array([(0,0),(0,1),(1,0),(1,1)])
 matrizY = np.array([(0,0),(0,1),(0,1),(1,1)])
 
-myPercepetron.train(matrizX,matrizY)
+perceptronOr.train(matrizX,matrizY[:,1])
+percetronAnd.train(matrizX,matrizY[:,0])
+
+
+
+#%%
+print("Teste percetron or")
+for row in matrizX:
+    print(perceptronOr.predict(row))
+
+print("Teste percetron And")
+for row in matrizX:
+    print(percetronAnd.predict(row))
+
+# In[]
+#gerar dados para o adaline
+#garantir q essa func esta certa
+def gerarDadosAdaline(qtd):
+    lista = []
+    listaInput =[]
+    a = np.sin(np.pi)
+    print(a)
+
+    def corrigir(a):
+        if abs(a) < 0.0001:
+            return 0
+        else:
+            return a
+
+    for p in np.linspace(-np.pi, 2*np.pi, qtd):
+        z = p
+        f1 = corrigir(np.sin(z))
+        f2 = corrigir(np.cos(z))
+        f3 = z
+        listaInput.append([f1, f2, f3])
+        output = -np.pi + 0.565*f1+2.657*f2+0.674*f3
+        lista.append([output])
+    return np.array(listaInput),np.array(lista)
+
+dadosX,DadosY = gerarDadosAdaline(8)
+print(dadosX)
+
+
+
+#%%
+#Adaline
+
+class MyAdaline(object):
+    def __init__(self, no_of_inputs, threshold=100, learning_rate=0.01,erro_delta_minimo=0.1):
+        self.threshold = threshold
+        self.learning_rate = learning_rate
+        self.weights = np.zeros(no_of_inputs + 1)
+        self.erro_delta_minimo = erro_delta_minimo
+        
+    # Modficar adaline para fazer ...
+    def predict(self, inputs):
+        # multipla as entradas pelo pesos
+        return np.sum(np.dot(inputs, self.weights[1:]) + self.weights[0])       
+      
+
+    def train(self, training_inputs, labels):
+        erroAnterior = 0
+        for _ in range(self.threshold):
+            erroMedio = 0
+            for inputs, label in zip(training_inputs, labels):
+                prediction = self.predict(inputs)
+                erro = 0.5 * ((label - prediction) * (label - prediction))
+                erroMedio += erro/(len(labels))
+                #print("_____________________")
+                #print("Entrada : ", inputs)
+                #print("Saida: ",prediction)
+                #print("Label: ",label)
+                self.printPesos()
+                self.weights[1:] += self.learning_rate * erro * inputs
+                self.weights[0] += self.learning_rate * erro
+                #print("corrigindo")
+                #self.printPesos()
+                #print("Erro Medio ", erroMedio)
+            print(abs((erroAnterior - erroMedio))) 
+            if abs((erroAnterior - erroMedio)) <= self.erro_delta_minimo:
+                break
+            else:
+                erroAnterior = erroMedio
+    
+    def printPesos(self):
+        print(self.weights)
+
+
+ada = MyAdaline(3,learning_rate=0.01,threshold=10)
+ada.train(dadosX,DadosY)
+
+
+for row in dadosX:
+    print(ada.predict(row))
+
+#%%
